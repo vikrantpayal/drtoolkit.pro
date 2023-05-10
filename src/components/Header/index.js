ad2cd52ca9acd61ce37496774e064c078df6954d
vkrntgit <vikrantpayal@protonmail.com>
2023-05-10 19:30:08 +0530

    Distilled menu to just Home and Search

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,16 +9,14 @@ class Header extends Component {
     <header>
       <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Dr. Toolkit <sup>pro</sup> </Link>
+          <Link className="navbar-brand" to="/home">Dr. Toolkit <sup>pro</sup> </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <NavLink to="/">Home</NavLink>
+              <NavLink to="/home">Home</NavLink>
               <NavLink to="/searchmedicines">Search Medicines</NavLink>
-              <NavLink to="/about">About</NavLink>
-              <NavLink to="/contact">Contact</NavLink>
             </ul>
           </div>
         </div>
