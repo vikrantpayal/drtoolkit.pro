552425129bfb712967bd3a3d2bbd3f6f57f4d291
vkrntgit <vikrantpayal@protonmail.com>
2023-05-10 19:32:52 +0530

    Added Dr. Abhi label to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,13 +5,28 @@ class Home extends Component {
   render() {
     return (
       <>
-        
+	<p/>	
         <h1>Home</h1>
         <p><br/>
-         Fast, simple tools for Allopathy practitioners across India. 
+         <h2>About Dr. Toolkit Pro</h2><br/>
+         Fast, simple tools for Allopathy practitioners across India.<br/>
+         This toolkit started with an idea - making life easier for busy healthcare professionals. 
+         The first tool we have built is a fast search.  
          Click on Search Medicines in the menu to get started.
-</p>
- 
+         </p>
+         <p>
+         The <b>Search Medicines</b> tool helps doctors quickly query
+         our database of medicines. You can search on ingredients, brand,
+         mode of administration, even prices. This helps doctors recommend
+         the best from the available options. The tool is designed with a 
+         focus on ergonomics and speed - no forms or buttons. See results
+         instantly, while you key in the search. 
+         </p>
+         <p>
+         We will be keeping the tools free through 2023 for the first hundred practitioners using Dr. Toolkit Pro. 
+         </p>
+         <br/>
+         <p><i>~ Dr. Abhishek Payal</i></p>
       </>
     )
   }
