9c8708af020dc5cc6d0b2eb6108343228c8dcb1e
vkrntgit <vikrantpayal@protonmail.com>
2023-05-10 10:22:04 +0530

    changed default page to SearchMeds

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component{
     render(){
        return( <BrowserRouter>
             <Header />
-
+                <SearchMedicines />
                 <main className="flex-shrink-0">
                     <div className="container">
                         <Routes>
